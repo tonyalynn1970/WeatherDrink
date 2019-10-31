e583a2b69c96a01f4ba3af6e8614daeb1564d06c
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-31 11:39:13 -0400

    added ids to the weather condition spans

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
     $(document).foundation();
-    console.log("hello")
+
+    let pHLocations = ["Atlanta, GA", "Tampa, Florida", "New York, NY", "Denver, CO"];
+    let pHConditions = ["wi-day-sunny", "wi-day-cloudy", "wi-day-snow-wind", "wi-day-thunderstorm"]
+
+    let condDisplay = pHConditions[randomize(0, pHConditions.length)];
+    console.log(condDisplay)
+    let tempDisplay = randomize(32, 100);
+    let locDisplay = pHLocations[randomize(0, pHLocations.length)];
+    $("#temperature").text(tempDisplay);
+    $("#location").text(locDisplay);
+
+    $("#condition").addClass(condDisplay)
+        //functions
+        // function for random
+    function randomize(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+
+    }
 })
