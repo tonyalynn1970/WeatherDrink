4b9f8bda5fc419fd0a72d1bbb4a318b00218b64c
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-10-31 12:32:59 -0400

    made it so the background is reflected based on the weather conditions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,16 +2,41 @@ $(document).ready(function() {
     $(document).foundation();
 
     let pHLocations = ["Atlanta, GA", "Tampa, Florida", "New York, NY", "Denver, CO"];
-    let pHConditions = ["wi-day-sunny", "wi-day-cloudy", "wi-day-snow-wind", "wi-day-thunderstorm"]
+    let pHConditions = [{
+            "description": "sun",
+            "conditionIcon": "wi-day-sunny",
+            "background": "sunny"
+        },
+
+        {
+            "description": "rain",
+            "conditionIcon": "wi-day-thunderstorm",
+            "background": "rainy"
+        },
+
+        {
+            "description": "snow",
+            "conditionIcon": "wi-day-snow-wind",
+            "background": "snowy"
+        },
+
+        {
+            "description": "cloud",
+            "conditionIcon": "wi-day-cloudy",
+            "background": "cloudy"
+        }
+    ]
+
+
 
     let condDisplay = pHConditions[randomize(0, pHConditions.length)];
-    console.log(condDisplay)
+
     let tempDisplay = randomize(32, 100);
     let locDisplay = pHLocations[randomize(0, pHLocations.length)];
     $("#temperature").text(tempDisplay);
     $("#location").text(locDisplay);
-
-    $("#condition").addClass(condDisplay)
+    $("body").addClass(condDisplay.background)
+    $("#condition").addClass(condDisplay.conditionIcon)
         //functions
         // function for random
     function randomize(min, max) {
